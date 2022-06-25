30b711ccfff3c6b6f160b4d38aba5d307375f184
OptorGames <99727012+OptorGames@users.noreply.github.com>
2022-06-25 21:33:57 +0300

    Add project files.

---
+++ Solution Example 1  Max/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solution_Example_1__Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
